6bf04d051f2ed7b03fb8e48d2a40823d2a69c775
BinaryGenius <105467963+BinaryGenius@users.noreply.github.com>
2022-10-19 00:12:28 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-# Bring back window buttons.
-gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+# Gnome Tweak Essentials
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close' # Gnome add this back for the love of...
+gsettings set org.gnome.mutter center-new-windows true # No good reason not to have.
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true # One of the coolest lazy features.
 
 # Because they're awesome.
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -15,3 +17,12 @@ gsettings set org.gnome.desktop.interface clock-format '12h'
 
 # Load keybinds from keybinds.ini.
 dconf load / < keybinds.ini
+
+# Speed of typing with the comfort of a GUI.
+gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+
+# Useful for notes or any time you need to create a file.
+mkdir Templates; cd Templates; touch MarkDown.md Shell\ Script.sh Text.txt
+
+# For finer control of volume
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 5
