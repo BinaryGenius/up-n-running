9e4bb15529054b69076574226d7764643a3915af
BinaryGenius <105467963+BinaryGenius@users.noreply.github.com>
2022-10-18 20:24:58 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,3 +6,9 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # Computers aren't the only things that sleep.
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+
+# Dark mode is the right mode.
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+# Why this isn't automatically set based on language I do not know.
+gsettings set org.gnome.desktop.interface clock-format '12h'
