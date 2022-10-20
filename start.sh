b1fbc345742d20670c4161828627a07219fa303f
BinaryGenius <105467963+BinaryGenius@users.noreply.github.com>
2022-10-20 12:45:48 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true # O
 
 # Because they're awesome.
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# For users
+# flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Computers aren't the only things that sleep.
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
